cf0c5f160d156f7b397cba334217b5775bd1aa6e
pamelabelendelvalle.veron.contractor.gdevtools.com <pamelabelendelvalle.veron.contractor@gdevtools.com>
2023-03-28 23:03:24 -0300

    add angular material & expansion list created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ExpansionListComponent } from './components/expansion-list/expansion-list.component';
+
+//////////////// Material ///////////////////
+import {MatExpansionModule} from '@angular/material/expansion';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpansionListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/expansion-list/expansion-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-expansion-list',
+  templateUrl: './expansion-list.component.html',
+  styleUrls: ['./expansion-list.component.css']
+})
+export class ExpansionListComponent {
+  panelOpenState = false;
+}
